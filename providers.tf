24ff41972d34c557c249d09dd1f9e11a18a6bf89
samuelinaboya <101844069+samuelinaboya@users.noreply.github.com>
2026-07-08 13:52:00 -0500

    Set up Terraform providers and workspace configuration
    Configure Terraform with required version and Google provider settings.

--- providers.tf
+++ providers.tf
@@ -1,1 +1,25 @@
+terraform {
+  required_version = ">= 1.3"
+  
+  cloud {
+    organization = "inaboya-tfc"
+
+    workspaces {
+      # Replacing 'name' with 'tags' allows GitHub Actions to dynamically pick the workspace
+      tags = ["azure"]
+    }
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.26.0, < 8"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = "us-east4"
+}
 
