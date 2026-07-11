964ce881cd92b29e1aba777807e84eb7238bc66d
samuelinaboya <101844069+samuelinaboya@users.noreply.github.com>
2026-07-11 15:49:16 -0500

    Rename service plan resource from 'example' to 'sp'

--- web-app.tf
+++ web-app.tf
@@ -8,7 +8,7 @@ resource "azurerm_container_registry" "acr" {
 }
 
 # 3. Create the App Service Plan
-resource "azurerm_service_plan" "example" {
+resource "azurerm_service_plan" "sp" {
   name                = "candle-store-sp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -21,7 +21,7 @@ resource "azurerm_linux_web_app" "webApp" {
   name                = "samuel-candle-store-app"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.example.id
+  service_plan_id     = azurerm_service_plan.sp.id
 
   app_settings = {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
