ce14473d85b225c9e3ba49af7ee6b49dea5c0a1b
samuelinaboya <101844069+samuelinaboya@users.noreply.github.com>
2026-07-11 19:36:44 -0500

    Add Azure resources for Linux web app deployment

--- web-app.tf
+++ web-app.tf
@@ -7,3 +7,50 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = false   # Disabled because we are using Managed Identity for security instead
 }
 
+# 3. Create the App Service Plan
+resource "azurerm_service_plan" "sp" {
+  name                = "candle-store-sp-dev"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
+}
+
+# 4. Deploy the Linux Web App
+resource "azurerm_linux_web_app" "webApp" {
+  name                = "samuel-candle-store-app"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.sp.id
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    always_on = true
+
+    # Tell App Service to authenticate using its Managed Identity
+    container_registry_use_managed_identity = true
+
+    application_stack {
+      docker_image_name   = "candle-store:latest"
+      # Dynamically grab the login server from the new ACR resource block above
+      docker_registry_url = "https://${azurerm_container_registry.acr.login_server}"
+    }
+  }
+}
+
+# 5. Dynamic Role Assignment using the created registry's ID
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_linux_web_app.webApp.identity[0].principal_id
+}
