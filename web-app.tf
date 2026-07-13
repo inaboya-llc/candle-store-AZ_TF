732bcbdc6ae8a69a8d0a21109a5e6bddee76353c
samuelinaboya <101844069+samuelinaboya@users.noreply.github.com>
2026-07-13 16:46:54 -0500

    Change SKU name from B1 to S1

--- web-app.tf
+++ web-app.tf
@@ -13,7 +13,7 @@ resource "azurerm_service_plan" "sp" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "S1"
   depends_on = [
     azurerm_resource_group.rg
   ]
