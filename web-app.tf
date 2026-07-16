c2e510fdac17537c40207070bb53d41e72dee429
samuelinaboya <101844069+samuelinaboya@users.noreply.github.com>
2026-07-16 18:42:51 -0500

    Replace hardcoded ACR name with variable

--- web-app.tf
+++ web-app.tf
@@ -1,6 +1,6 @@
 # 2. Automatically Create the Azure Container Registry
 resource "azurerm_container_registry" "acr" {
-  name                = "candleRegistry" # Must be globally unique and alphanumeric
+  name                = var.acr_name # Must be globally unique and alphanumeric
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic" # Basic is perfect for development and budget-friendly
