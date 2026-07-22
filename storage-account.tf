369f9c9a51ba78c28b6732f3bf11558690594031
samuelinaboya <inaboya_samuel@yahoo.com>
2026-07-22 17:18:19 -0500

    az storage 3

--- storage-account.tf
+++ storage-account.tf
@@ -1,19 +1,23 @@
-# # Create an Azure Storage Account
-# resource "azurerm_storage_account" "candlestore_sa" {
-#   name                     = "candlestoresa01" # Must be globally unique, 3-24 lowercase letters/numbers
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   location                 = azurerm_resource_group.rg.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS" # Options: LRS, GRS, RAGRS, ZRS
+# Create an Azure Storage Account
+resource "azurerm_storage_account" "candlestore_sa" {
+  name                     = "candlestoresa01" # Must be globally unique, 3-24 lowercase letters/numbers
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS" # Options: LRS, GRS, RAGRS, ZRS
 
-#   tags = {
-#     environment = "dev"
-#   }
-# }
+  tags = {
+    environment = "dev"
+  }
 
-# # Create a Blob Storage Container inside the Storage Account
-# resource "azurerm_storage_container" "candlestore_container" {
-#   name                  = "candlestore-blobs"
-#   storage_account_name  = azurerm_storage_account.candlestore_sa.name
-#   container_access_type = "private" # Options: private, blob, container
-# }
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
+}
+
+# Create a Blob Storage Container inside the Storage Account
+resource "azurerm_storage_container" "candlestore_container" {
+  name                  = "candlestore-blobs"
+  storage_account_name  = azurerm_storage_account.candlestore_sa.name
+  container_access_type = "private" # Options: private, blob, container
+}
