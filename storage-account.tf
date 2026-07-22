2c98b3e134ac7596971115e4c9736589f60183c6
samuelinaboya <101844069+samuelinaboya@users.noreply.github.com>
2026-07-22 16:25:44 -0500

    Add Azure Storage Account and Blob Container resources

--- storage-account.tf
+++ storage-account.tf
@@ -1,1 +1,19 @@
+# Create an Azure Storage Account
+resource "azurerm_storage_account" "candlestore_sa" {
+  name                     = "candlestoresa01" # Must be globally unique, 3-24 lowercase letters/numbers
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS" # Options: LRS, GRS, RAGRS, ZRS
 
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Create a Blob Storage Container inside the Storage Account
+resource "azurerm_storage_container" "candlestore_container" {
+  name                  = "candlestore-blobs"
+  storage_account_name  = azurerm_storage_account.candlestore_sa.name
+  container_access_type = "private" # Options: private, blob, container
+}
